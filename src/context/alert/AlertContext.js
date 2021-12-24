3220811f12fd8af373ae794360e760da84c7f7f9
Dj Hammond <59342048+Dj-glitch@users.noreply.github.com>
2021-12-24 10:56:58 -0600

    add setAlert to AlertContext.provider

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -18,7 +18,9 @@ export const AlertProvider = ({ children }) => {
 		setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000)
 	}
 
-	return <AlertContext.Provider value={{ alert: state }}>{children}</AlertContext.Provider>
+	return (
+		<AlertContext.Provider value={{ alert: state, setAlert }}>{children}</AlertContext.Provider>
+	)
 }
 
 export default AlertContext
